4eb6ea1d7fa8117932db987c9a3beeeea0e7475b
Rabie <rabbyknyan@gmail.com>
2026-02-03 22:10:29 +0300

    Update config.py
    change the names of the options form Arabic to English

--- ScanLogic v1.0/config.py
+++ ScanLogic v1.0/config.py
@@ -2,8 +2,8 @@ import argparse
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Professional Port Scanner")
-    parser.add_argument("target", help="الهدف (IP)")
-    parser.add_argument("-p", "--ports", default="1-1024", help="نطاق المنافذ")
-    parser.add_argument("-t", "--threads", type=int, default=100, help="عدد الخيوط")
-    parser.add_argument("-o", "--output", help="اسم ملف التقرير")
+    parser.add_argument("target", help="target (IP)")
+    parser.add_argument("-p", "--ports", default="1-1024", help="To Determine the range of ports")
+    parser.add_argument("-t", "--threads", type=int, default=100, help="Thread Number")
+    parser.add_argument("-o", "--output", help="Report File name")
     return parser.parse_args()
